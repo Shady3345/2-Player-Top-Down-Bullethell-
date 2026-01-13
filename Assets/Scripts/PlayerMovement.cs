a80e16393a201b95ace99e1ac5a1e3c507ae5fc2
canoezbal@gmail.com <canoezbal@gmail.com>
2026-01-13 13:24:09 +0100

    character moveable without any sprites

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDir;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0f; // Ensure no gravity for top-down movement
+    }
+
+    void Update()
+    {
+        InputManager();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    void InputManager()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.linearVelocity = moveDir * moveSpeed;
+    }
+}
